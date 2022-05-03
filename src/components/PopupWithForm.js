9d8496f13ced79190a8d53782f6b0275e99b513d
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-05-03 13:43:38 +0200

    fix

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,20 +1,25 @@
-function PopupWithForm(props) {
+function PopupWithForm({
+  name,
+  isOpen,
+  container,
+  title,
+  button,
+  onClose,
+  children,
+}) {
   return (
-    <section
-      className={`popup popup_${props.name} ${props.isOpen && "popup_enable"}`}
-    >
-      <div className={`popup__container ${props.container}`}>
-        <h2 className="popup__title">{props.title}</h2>
-        <form className="popup__form" name={props.name} noValidate>
-          {props.children}
-
+    <section className={`popup popup_${name} ${isOpen && "popup_enable"}`}>
+      <div className={`popup__container ${container}`}>
+        <h2 className="popup__title">{title}</h2>
+        <form className="popup__form" name={name} noValidate>
+          {children}
           <button className="popup__save" type="submit">
-            {props.button}
+            {button}
           </button>
         </form>
         <button
           className="popup__close"
-          onClick={props.onClose}
+          onClick={onClose}
           type="reset"
           aria-label="Закрыть"
         ></button>
